566d211057990cf05f87405c0bd2335e6a7f86f6
Dylan Malla <51140000+D-Malla@users.noreply.github.com>
2022-11-25 10:58:42 -0800

    Added useful debug macro file, and created item base class

--- Source/Wilderness/Private/Items/Item.cpp
+++ Source/Wilderness/Private/Items/Item.cpp
@@ -8,17 +8,27 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	RunningTime += DeltaTime;
 }
-
